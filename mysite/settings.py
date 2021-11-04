2b87ae8b78de8c501e61ea0a823e624c049e5c28
Tobiasz Kędzierski <tobiaszkedzierski@gmail.com>
2021-11-04 13:05:03 +0100

    Add support for .env file and Google Secret Manager

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,17 +1,41 @@
+import io
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import environ
+from google.cloud import secretmanager
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+env = environ.Env(DEBUG=(bool, False))
+env_file = os.path.join(BASE_DIR, ".env")
+
+if os.path.isfile(env_file):
+    env.read_env(env_file)
+# Pull secrets from Google Secret Manager
+elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+    env.read_env(io.StringIO(payload))
+else:
+    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4uncnlo=m0l1hx7cz@+%mxbqdkcq3m)%iwcqbtq943w&87syek"
+SECRET_KEY = env(
+    "SECRET_KEY",
+    default="django-insecure-4uncnlo=m0l1hx7cz@+%mxbqdkcq3m)%iwcqbtq943w&87syek",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
+
 
 ALLOWED_HOSTS = []
 
@@ -62,12 +86,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+default_sqlite3_database = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+DATABASES = {"default": env.db("DATABASE_URL", default=default_sqlite3_database)}
+
+# If the flag as been set, configure to use proxy
+if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
+    DATABASES["default"]["PORT"] = 5432
 
 
 # Password validation
